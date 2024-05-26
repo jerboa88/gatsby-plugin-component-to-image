fa90d5eaf0e6cd7d3d466831736128fb056b1b57
John Goodliff <jerboa88@users.noreply.github.com>
2024-05-25 22:44:41 -0600

    Replace manual validation functions with Joi. Add function for setting default options

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,20 @@
 const { setReporter } = require('./src/logger');
 const { setGatsbyCreatePageFunction } = require('./src/config');
 const { generateImages } = require('./src/generator');
+const { setJoi } = require('./src/validator');
 
 // Save the reporter and createPage function for later use
 exports.onPluginInit = async ({ reporter, actions: { createPage } }) => {
 	setReporter(reporter);
 	setGatsbyCreatePageFunction(createPage);
+};
+
+// Validate plugin options defined in gatsby-config.js
+exports.pluginOptionsSchema = ({ Joi }) => {
+	setJoi(Joi);
+
+	// TODO: Add validation for plugin options
+	return Joi.object({});
 };
 
 // Generate images after pages have been built
