bfb7aa5b5df147dda9f794ac1c4772f3587d96ac
John Goodliff <jerboa88@users.noreply.github.com>
2024-06-03 17:57:22 -0600

    Disable ts-jest TypeScript errors

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,14 @@ module.exports = {
 	collectCoverageFrom: ['src/**/*.ts'],
 	coverageDirectory: 'coverage',
 	coverageProvider: 'v8',
+	transform: {
+		'^.+\\.ts$': [
+			'ts-jest',
+			{
+				diagnostics: {
+					warnOnly: true,
+				},
+			},
+		],
+	},
 };
