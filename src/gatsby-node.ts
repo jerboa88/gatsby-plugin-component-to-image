7d2cb1b3a8f892c97839fcb5cd6365a55e71903c
John Goodliff <jerboa88@users.noreply.github.com>
2024-06-02 19:58:16 -0600

    Add types for gatsby-node.ts

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -3,28 +3,37 @@ import { setGatsbyCreatePageFunction, setDefaultOptions } from './config';
 import { generateImages } from './generator';
 import { setJoi, getPluginOptionsSchema } from './validator';
 import { prettify } from './utilities';
+import type { GatsbyNode } from 'gatsby';
 
 // Save the reporter and createPage function for later use
-export const onPluginInit = async ({ reporter, actions: { createPage } }) => {
+export const onPluginInit: GatsbyNode['onPluginInit'] = async ({
+	reporter,
+	actions: { createPage },
+}) => {
 	setReporter(reporter);
 	setGatsbyCreatePageFunction(createPage);
 };
 
 // Save Joi for later and return the schema for plugin options validation by Gatsby
-export const pluginOptionsSchema = ({ Joi }) => {
+export const pluginOptionsSchema: GatsbyNode['pluginOptionsSchema'] = ({
+	Joi,
+}) => {
 	setJoi(Joi);
 
 	return getPluginOptionsSchema();
 };
 
 // Get plugin options from gatsby-config.js and set default options
-export const onPreBootstrap = async (_, pluginOptions) => {
+export const onPreBootstrap: GatsbyNode['onPreBootstrap'] = async (
+	_,
+	pluginOptions,
+) => {
 	const defaultOptions = setDefaultOptions(pluginOptions);
 
 	info(`Default options set to:\n${prettify(defaultOptions)}`);
 };
 
 // Generate images after pages have been built
-export const onPostBuild = async () => {
+export const onPostBuild: GatsbyNode['onPostBuild'] = async () => {
 	await generateImages();
 };
