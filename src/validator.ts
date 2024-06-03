1c1c4c5834a71c33bd7faa655388197fb8dedb0c
John Goodliff <jerboa88@users.noreply.github.com>
2024-06-02 21:18:49 -0600

    Add types to validator.ts

--- src/validator.ts
+++ src/validator.ts
@@ -1,116 +1,158 @@
-import { isUndefined } from './utilities';
+import type { PluginOptions } from 'gatsby';
+import type { PluginOptionsSchemaJoi } from 'gatsby-plugin-utils';
+import type { DefaultOptions, JobOptions } from './types';
+import { assertIsDefined } from './utilities';
 
-let Joi;
-let schema;
+// Types
+type BooleanSchema = ReturnType<PluginOptionsSchemaJoi['boolean']>;
+type NumberSchema = ReturnType<PluginOptionsSchemaJoi['number']>;
+type ObjectSchema = ReturnType<PluginOptionsSchemaJoi['object']>;
+type StringSchema = ReturnType<PluginOptionsSchemaJoi['string']>;
+type Schemas = {
+	verbose: BooleanSchema;
+	pagePath: StringSchema;
+	imagePath: StringSchema;
+	component: StringSchema;
+	context: ObjectSchema;
+	size: ObjectSchema;
+	type: StringSchema;
+	quality: NumberSchema;
+	optimizeForSpeed: BooleanSchema;
+};
+type SchemaTransformFunc<T> = (schema: T) => T;
+type SchemaTransformFuncs = {
+	[K in keyof Schemas]?: SchemaTransformFunc<Schemas[K]>;
+};
 
-function getSchemaWithDefaultValue(schema, defaultValue) {
-	if (isUndefined(defaultValue)) {
-		return schema;
+// Constants
+
+const FILE_TYPES = ['png', 'jpeg', 'webp', 'pdf'] as const;
+const MIN_QUALITY = 0 as const;
+const MAX_QUALITY = 100 as const;
+const MIN_SIZE = 1 as const;
+const MAX_SIZE = 16383 as const;
+
+// Runtime vars
+
+let joi: PluginOptionsSchemaJoi | undefined = undefined;
+let schemas: Schemas | undefined = undefined;
+
+function validateOptions<T>(
+	options: Partial<T> | PluginOptions,
+	fallbackOptions: DefaultOptions,
+	schemaTransformFuncs: Partial<{
+		[K in keyof Schemas]: (schema: Schemas[K]) => Schemas[K];
+	}>,
+) {
+	assertIsDefined(joi);
+	assertIsDefined(schemas);
+
+	const resultMap: Partial<T> = {};
+
+	for (const [optionName, schemaTransformFunc] of Object.entries(
+		schemaTransformFuncs,
+	)) {
+		const value = options?.[optionName as keyof typeof options];
+		const fallbackValue =
+			fallbackOptions?.[optionName as keyof typeof fallbackOptions];
+		const schema = schemas[optionName as keyof Schemas];
+		const newSchema = schemaTransformFunc(schema);
+
+		resultMap[optionName as keyof T] = joi.attempt(
+			value ?? fallbackValue,
+			newSchema,
+		);
 	}
 
-	return schema.default(defaultValue);
+	return resultMap as T;
 }
 
-export function setJoi(newJoi) {
-	Joi = newJoi;
+export function setJoi(newJoi: PluginOptionsSchemaJoi) {
+	joi = newJoi;
 
-	schema = {
-		verbose: Joi.boolean().label('verbose'),
-		pagePath: Joi.string().required().label('pagePath'),
-		imagePath: Joi.string().required().label('imagePath'),
-		component: Joi.string().label('component'),
-		context: Joi.object().label('context'),
-		size: Joi.object({
-			width: Joi.number().integer().min(1).max(16383).label('width'),
-			height: Joi.number().integer().min(1).max(16383).label('height'),
+	schemas = {
+		verbose: joi.boolean().label('verbose'),
+		pagePath: joi.string().required().label('pagePath'),
+		imagePath: joi.string().required().label('imagePath'),
+		component: joi.string().label('component'),
+		context: joi.object().label('context'),
+		size: joi.object({
+			width: joi.number().integer().min(MIN_SIZE).max(MAX_SIZE).label('width'),
+			height: joi
+				.number()
+				.integer()
+				.min(MIN_SIZE)
+				.max(MAX_SIZE)
+				.label('height'),
 		}),
-		type: Joi.string().valid('png', 'jpeg', 'webp', 'pdf').label('type'),
-		quality: Joi.number().integer().min(0).max(100).label('quality'),
-		optimizeForSpeed: Joi.boolean().label('optimizeForSpeed'),
+		type: joi
+			.string()
+			.valid(...FILE_TYPES)
+			.label('type'),
+		quality: joi
+			.number()
+			.integer()
+			.min(MIN_QUALITY)
+			.max(MAX_QUALITY)
+			.label('quality'),
+		optimizeForSpeed: joi.boolean().label('optimizeForSpeed'),
 	};
 }
 
 export function getPluginOptionsSchema() {
-	return Joi.object({
-		verbose: schema.verbose,
-		component: schema.component,
-		context: schema.context,
-		size: schema.size,
-		type: schema.type,
-		quality: schema.quality,
-		optimizeForSpeed: schema.optimizeForSpeed,
+	assertIsDefined(joi);
+	assertIsDefined(schemas);
+
+	return joi.object({
+		verbose: schemas.verbose,
+		component: schemas.component,
+		context: schemas.context,
+		size: schemas.size,
+		type: schemas.type,
+		quality: schemas.quality,
+		optimizeForSpeed: schemas.optimizeForSpeed,
 	});
 }
 
-export function validateDefaultOptions(newDefaultOptions, defaultOptions) {
-	// Build Joi schemas
-	const newSchema = {
-		verbose: undefined,
-		component: undefined,
-		context: undefined,
-		size: undefined,
-		type: undefined,
-		quality: undefined,
-		optimizeForSpeed: undefined,
+export function validateDefaultOptions(
+	newDefaultOptions: PluginOptions | Partial<DefaultOptions>,
+	defaultOptions: DefaultOptions,
+): DefaultOptions {
+	const schemaTransformFuncs: SchemaTransformFuncs = {
+		verbose: (schema) => schema,
+		component: (schema) => schema,
+		context: (schema) => schema,
+		size: (schema) => schema,
+		type: (schema) => schema,
+		quality: (schema) => schema,
+		optimizeForSpeed: (schema) => schema,
 	};
 
-	for (const key in schema) {
-		newSchema[key] = getSchemaWithDefaultValue(
-			schema[key],
-			defaultOptions?.[key],
-		);
-	}
-
-	// Validate and return plugin options
-	return {
-		verbose: Joi.attempt(newDefaultOptions?.verbose, newSchema.verbose),
-		component: Joi.attempt(newDefaultOptions?.component, newSchema.component),
-		context: Joi.attempt(newDefaultOptions?.context, newSchema.context),
-		size: Joi.attempt(newDefaultOptions?.size, newSchema.size),
-		type: Joi.attempt(newDefaultOptions?.type, newSchema.type),
-		quality: Joi.attempt(newDefaultOptions?.quality, newSchema.quality),
-		optimizeForSpeed: Joi.attempt(
-			newDefaultOptions?.optimizeForSpeed,
-			newSchema.optimizeForSpeed,
-		),
-	};
+	return validateOptions<DefaultOptions>(
+		newDefaultOptions,
+		defaultOptions,
+		schemaTransformFuncs,
+	);
 }
 
-export function validateJobOptions(newJobOptions, defaultOptions) {
-	// Build Joi schemas
-	const newSchema = {
-		pagePath: undefined,
-		imagePath: undefined,
-		component: undefined,
-		context: undefined,
-		size: undefined,
-		type: undefined,
-		quality: undefined,
-		optimizeForSpeed: undefined,
+export function validateJobOptions(
+	newJobOptions: Partial<JobOptions>,
+	defaultOptions: DefaultOptions,
+): JobOptions {
+	const schemaTransformFuncs: SchemaTransformFuncs = {
+		pagePath: (schema) => schema.required(),
+		imagePath: (schema) => schema.required(),
+		component: (schema) => schema.required(),
+		context: (schema) => schema,
+		size: (schema) => schema.required(),
+		type: (schema) => schema.required(),
+		quality: (schema) => schema.required(),
+		optimizeForSpeed: (schema) => schema.required(),
 	};
 
-	for (const key in schema) {
-		newSchema[key] = getSchemaWithDefaultValue(
-			schema[key],
-			defaultOptions?.[key],
-		);
-	}
-
-	// Validate and return job options
-	return {
-		pagePath: Joi.attempt(newJobOptions?.pagePath, newSchema.pagePath),
-		imagePath: Joi.attempt(newJobOptions?.imagePath, newSchema.imagePath),
-		component: Joi.attempt(
-			newJobOptions?.component,
-			schema.component.required(),
-		),
-		context: Joi.attempt(newJobOptions?.context, newSchema.context),
-		size: Joi.attempt(newJobOptions?.size, newSchema.size),
-		type: Joi.attempt(newJobOptions?.type, newSchema.type),
-		quality: Joi.attempt(newJobOptions?.quality, newSchema.quality),
-		optimizeForSpeed: Joi.attempt(
-			newJobOptions?.optimizeForSpeed,
-			newSchema.optimizeForSpeed,
-		),
-	};
+	return validateOptions<JobOptions>(
+		newJobOptions,
+		defaultOptions,
+		schemaTransformFuncs,
+	);
 }
